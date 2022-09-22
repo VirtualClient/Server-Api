2d60d1418d3663b0b512284f75e1842732ab10ff
David Mayr <davidliebtkekse@gmail.com>
2022-09-22 08:33:53 +0200

    fix

--- api/velocity-api/build.gradle.kts
+++ api/velocity-api/build.gradle.kts
@@ -16,7 +16,7 @@ repositories {
 dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.0")
-    implementation(project(":api:common-api"))
+    api(project(":api:common-api"))
     compileOnly("com.velocitypowered:velocity-api:3.1.1-SNAPSHOT")
 }
 
